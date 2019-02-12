1ebd32ed0007cae5d5e585c3ad2e691b3c376957
al160021 <46726435+al160021@users.noreply.github.com>
2019-02-12 13:26:24 -0700

    Add files via upload

---
+++ ejercicio10_ecuacion2.java
@@ -0,0 +1,9 @@
+//Nombre: Gerardo Alejandro Piña Puentes Fecha : 02/12/2019 Hora : 1:00
+public class ejercicio10_ecuacion2 {
+ 
+    public static void main(String[] args) {
+
+         System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));   
+        
+    }
+}
