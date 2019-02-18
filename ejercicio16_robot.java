a43596de3c329e7b1f8fa879fdab56b7a211ceb8
al160021 <46726435+al160021@users.noreply.github.com>
2019-02-17 20:44:48 -0700

    Add files via upload

---
+++ ejercicio16_robot.java
@@ -0,0 +1,12 @@
+//Nombre : Gerardo Alejandro Piña Puentes fecha : 17/02/2019 hora: 8:30
+public class ejercicio16_robot {
+
+ 	public static void main(String[] args)
+		{
+	System.out.println(" +''''''+ ");
+	System.out.println("[| °  ° |]");
+	System.out.println(" |  ^   | ");
+	System.out.println(" | '-'  | ");
+	System.out.println(" +------+ ");
+ }
+}
